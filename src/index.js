b48ae64df6b0c00f2455c79cb7914cc1b7c3ebd1
Anthony Sychev <hello@dm211.com>
2023-10-25 20:56:03 +0200

    todo: rewrite all class

---
+++ src/index.js
@@ -0,0 +1,12 @@
+/*
+█▀ █▄█ █▀▀ █░█ █▀▀ █░█
+▄█ ░█░ █▄▄ █▀█ ██▄ ▀▄▀
+
+Author: <Anton Sychev> (anton at sychev dot xyz) 
+index.mjs (c) 2023 
+Created:  2023-10-26 02:43:48 
+Desc: index file for rocket-store-node
+Docs: documentation
+*/
+
+//TODO: rebuild all module
